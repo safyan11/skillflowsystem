2990533278be96302b83f4b442eebb2300754f1d
riss group <wahadsafyan410@gmail.com>
2025-08-19 21:05:42 +0500

    first commit

--- build/about.php
+++ build/about.php
@@ -6,11 +6,7 @@
 
 
 
-<section class="h-96 bg-fixed bg-center bg-no-repeat bg-cover" style="background-image: url('./assets/img/about.png');">
-  <div class="h-96 flex items-center justify-center bg-black bg-opacity-80">
-    <h1 class="md:text-4xl text-2xl font-bold text-white">About us</h1>
-  </div>
-</section>
+
 
 <!-- about us  -->
 <section class="xl:px-20 lg:px-10 px-5 xl:py-20 py-10 ">
