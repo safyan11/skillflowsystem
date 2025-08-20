91ea7c15e819c7d79690824c71100c01bc2c03ac
riss group <wahadsafyan410@gmail.com>
2025-08-20 15:29:43 +0500

    first commit

--- build/courses.php
+++ build/courses.php
@@ -69,7 +69,30 @@
 
 <!-- footer  -->
  
-
+<footer class="bg-black lg:py-20 py-10 xl:px-20 lg:px-10 px-5">
+  <div class="flex md:justify-between justify-center items-center md:flex-row flex-col">
+    <div class="flex flex-col justify-center">
+      <img src="./assets/img/" alt="">
+      <h1 class="text-2xl font-bold text-white">Learning Hub</h1>
+    </div>
+    <div class="pt-5 md:pt-0">
+       <h1 class="md:text-2xl text-xl font-bold text-white">Quick Links</h1>
+      <div class="flex flex-col justify-center items-center pt-6 space-y-4">
+        <a href="index.html" class="text-white font-medium text-base hover:underline">Home</a>
+        <a href="courses.html" class="text-white font-medium text-base hover:underline">Course</a>
+        <a href="about.html" class="text-white font-medium text-base hover:underline">About</a>
+        <a href="contact.html" class="text-white font-medium text-base hover:underline">Contact</a>
+        </div>
+    </div>
+    
+    <div class="space-y-4 flex flex-col justify-center items-center pt-5 md:pt-0">
+      <h1 class="md:text-2xl text-xl font-bold text-white">Contact Info</h1>
+      <p class="text-white text-base "><i class="fa-solid fa-location-dot"></i>&nbsp; Lahore, Pakistan</p>
+      <p class="text-white text-base "><i class="fa-solid fa-envelope"></i>&nbsp; learninghub@gmail.com</p>
+      <p class="text-white text-base "><i class="fa-solid fa-phone"></i>&nbsp; 0562929978</p>
+    </div>
+  </div>
+</footer>
 
 
   <script src="https://kit.fontawesome.com/a2ada4947c.js" crossorigin="anonymous"></script>
